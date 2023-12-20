ffa13ee647bc595d49e42ebad959eb37f0848e7e
mila2999 <mila.savonik@gmail.com>
2023-12-20 14:38:01 +0200

    Defining Routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import HomePage from './pages/Home';
+const router = createBrowserRouter([{ path: '/', element: <HomePage /> }]);
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;

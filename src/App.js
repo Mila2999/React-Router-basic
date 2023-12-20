c05ef3ef86540f205e79b5d953868bff5e529675
mila2999 <mila.savonik@gmail.com>
2023-12-20 14:51:12 +0200

    Added second route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import HomePage from './pages/Home';
-const router = createBrowserRouter([{ path: '/', element: <HomePage /> }]);
+import ProductsPage from './pages/Products';
+
+const router = createBrowserRouter([
+  { path: '/', element: <HomePage /> },
+  { path: '/products', element: <ProductsPage /> },
+]);
 
 function App() {
   return <RouterProvider router={router} />;
